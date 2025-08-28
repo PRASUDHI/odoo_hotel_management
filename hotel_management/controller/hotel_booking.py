db6e111563a03c11544782a46dbf53786bb3ce68
PRASUDHI <prasu1602@gmail.com>
2025-08-28 17:31:27 +0530

    [UPDT] first commit

--- hotel_management/controller/hotel_booking.py
+++ hotel_management/controller/hotel_booking.py
@@ -4,30 +4,26 @@ from odoo.http import request
 
 class WebsiteHotelBooking(http.Controller):
 
-
-    @http.route(['/hotel/booking/submit'], type='http', auth="public", methods=['POST'], website=True, csrf=False)
+    @http.route(['/hotel/booking/submit'], type='http', auth="user", methods=['POST'], website=True, csrf=False)
     def hotel_booking_submit(self, **post):
-        name = post.get('name')
+        print(post)
         days = int(post.get('expected_days', 1))
+        bed = post.get('bed')
 
-        """ Save booking when user submits the form """
-        guest = request.env['res.partner'].sudo().create({
-            'name': name,
-            'email': post.get('email'),
-            'phone': post.get('phone'),
-            'is_hotel_guest': True,
-        })
+
+        user = request.env.user
+        partner = user.partner_id
+
 
         booking = request.env['hotel.accommodation'].sudo().create({
-            'guest_id': guest.id,
-            # 'bed':int(post.get('bed')),
-            # 'facility_ids':int(post.get('facility')),
-            # 'rooms_id': int(post.get('room_id')),
+            'guest_id': partner.id,
+            'bed': bed,
             'expected_days': days,
             'number_of_guests': int(post.get('guests')),
         })
-        # return request.redirect('/thank-you-page')
+
+
         return request.render("hotel_management.template_hotel_booking_success", {
             'booking': booking,
+            'partner': partner,
         })
-        
