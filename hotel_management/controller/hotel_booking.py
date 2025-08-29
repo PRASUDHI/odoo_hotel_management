55af00086d65dbacf1087f228ca2b15ee3502aa4
PRASUDHI <prasu1602@gmail.com>
2025-08-29 17:30:45 +0530

    [UPDT] first commit

--- hotel_management/controller/hotel_booking.py
+++ hotel_management/controller/hotel_booking.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from odoo import http
 from odoo.http import request
 
@@ -9,17 +11,25 @@ class WebsiteHotelBooking(http.Controller):
         print(post)
         days = int(post.get('expected_days', 1))
         bed = post.get('bed')
+        # room_id =post.get('room_id')
 
 
         user = request.env.user
         partner = user.partner_id
+        booking_time_str = post.get('booking_time')
+        booking_time = datetime.strptime(booking_time_str, "%Y-%m-%dT%H:%M") if booking_time_str else False
+        # facility_ids = request.httprequest.form.getlist('facility_ids')
+
+
 
 
         booking = request.env['hotel.accommodation'].sudo().create({
             'guest_id': partner.id,
             'bed': bed,
             'expected_days': days,
-            'number_of_guests': int(post.get('guests')),
+            'booking_time': booking_time,
+            # 'facility_ids': facility_ids,
+            # 'rooms_id':room_id
         })
 
 
